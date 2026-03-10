9f7d8150b39fca72c7c3ee463b05a89a6f46937a
halldorvalberg <halldor.valberg@hotmail.com>
2026-03-10 15:36:09 +0000

    add test paragraph to see

--- frontend/app/dev/page.tsx
+++ frontend/app/dev/page.tsx
@@ -11,6 +11,7 @@ export default async function DevPage() {
 
   return (
     <main className={styles.main}>
+      <p>This is a test to see if the automatic deployment on Azure VM works correctly.</p>
       <h1 className={styles.title}>Verkbók</h1>
       <DevlogClient initialItems={items} total={total} pageSize={PAGE_SIZE} />
     </main>
